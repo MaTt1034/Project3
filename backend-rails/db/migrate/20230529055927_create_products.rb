97453dd6d3922f6ebb03261ce234946fb61932a8
edanmicto5 <edanmicto5@gmail.com>
2023-05-29 16:02:10 +1000

    Adding Product scaffold (rails g scaffold Product name description category price:decimal)

--- backend-rails/db/migrate/20230529055927_create_products.rb
+++ backend-rails/db/migrate/20230529055927_create_products.rb
@@ -1,6 +1,6 @@
-class CreateItems < ActiveRecord::Migration[7.0]
+class CreateProducts < ActiveRecord::Migration[7.0]
   def change
-    create_table :items do |t|
+    create_table :products do |t|
       t.string :name
       t.string :description
       t.string :category
--- backend-rails/db/schema.rb
+++ backend-rails/db/schema.rb
@@ -10,14 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_22_055951) do
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "category"
-    t.decimal "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
