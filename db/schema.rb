6fe91c869a6909961901150ddf110f263dac0132
edanmicto5 <edanmicto5@gmail.com>
2023-05-29 17:07:26 +1000

    Adding user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_055927) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_070244) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.string "category"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "email"
+    t.string "phone_number"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
