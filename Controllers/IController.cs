2f4d271243098d66d1b3bd71cf4c2579810a7ad1
edanmicto5 <edanmicto5@gmail.com>
2023-05-30 22:32:10 +1000

    Adding controller interface

---
+++ Controllers/IController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Project3.Controllers
+{
+	public interface IController
+	{
+		// gets all records
+		public void Index();
+
+		// shows an individual record
+		public void Show(int id);
+
+		public void Edit(int id);
+
+		public void Create();
+
+		public void Update(int id);
+
+		public void Destroy(int id);
+	}
+}
